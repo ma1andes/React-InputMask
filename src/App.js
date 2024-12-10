4b9c174580a58d1400cf90cfe3423b8edb70fa7f
Your Name <you@example.com>
2024-12-10 13:58:01 +0300

    замечания исправлены

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import './App.css';
-import HeaderForm from './components/HeaderForm/HeaderForm';
-import ButtonSend from './components/ButtonSend/ButtonSend';
-import PhoneNumberInput from './components/InputMask/InputMask';
-import DataInput from './components/DataInput/DataInput';
+import React, { useState } from "react";
+import "./App.css";
+import HeaderForm from "./components/HeaderForm/HeaderForm";
+import ButtonSend from "./components/ButtonSend/ButtonSend";
+import PhoneNumberInput from "./components/InputMask/InputMask";
+import DataInput from "./components/DataInput/DataInput";
 
 function App() {
   const [formData, setFormData] = useState({
@@ -11,16 +11,14 @@ function App() {
     lastName: "",
     phone: "",
   });
-  const [isPhoneComplete, setIsPhoneComplete] = useState(false); // Статус заполнения номера
+  const [isPhoneComplete, setIsPhoneComplete] = useState(true);
 
-  // Проверяем, что все поля формы заполнены
   const isFormValid =
     formData.firstName &&
     formData.lastName &&
     formData.phone &&
     isPhoneComplete;
 
-  // Обновляем данные формы
   const handleDataChange = (updatedData) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -28,39 +26,28 @@ function App() {
     }));
   };
 
-  // Обновляем данные номера телефона и его статус
   const handlePhoneChange = (phone, isComplete) => {
     handleDataChange({ phone });
-    setIsPhoneComplete(isComplete); // Обновляем статус валидности номера
+    setIsPhoneComplete(isComplete);
   };
 
-  // Обработка отправки формы
   const handleSubmit = () => {
-    console.log('Отправленные данные:', formData);
+    console.log("Отправленные данные:", formData);
     setFormData({
       firstName: "",
       lastName: "",
       phone: "",
     });
-    setIsPhoneComplete(false); // Сбрасываем состояние валидности номера
+    setIsPhoneComplete(false); // Устанавливаем значение в true для нового ввода
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       <HeaderForm />
-      <div className='formInput'>
-        <DataInput 
-          formData={formData}
-          onDataChange={handleDataChange} 
-        />
-        <PhoneNumberInput 
-          phone={formData.phone}
-          onPhoneChange={handlePhoneChange}
-        />
-        <ButtonSend 
-          onSubmit={handleSubmit} 
-          isDisabled={!isFormValid} 
-        />
+      <div className="formInput">
+        <DataInput formData={formData} onDataChange={handleDataChange} />
+        <PhoneNumberInput phone={formData.phone} onPhoneChange={handlePhoneChange} />
+        <ButtonSend onSubmit={handleSubmit} isDisabled={!isFormValid} />
       </div>
     </div>
   );
